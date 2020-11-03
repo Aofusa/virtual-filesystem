fc0d81f7d831f3c61c9680651fc59bb473030aa0
Aofusa <5000enPCinAOFUSA@gmail.com>
2020-11-04 00:01:57 +0900

    update: シェル一通り実装

--- src/main.rs
+++ src/main.rs
@@ -193,7 +193,7 @@ fn run(shell: &mut Shell, buffer: &Arg) {
     let command = *iter.next().unwrap();
 
     if command == ":?" {
-        println!("to stop, press Ctrl + c");
+        println!("to stop, press Ctrl + c or type exit");
         println!("Command list");
         println!("  ls");
         println!("  cd [directory]");
@@ -201,17 +201,81 @@ fn run(shell: &mut Shell, buffer: &Arg) {
         println!("  mkdir [directory]");
         println!("  touch [file]");
         println!("  read [file]");
-        println!("  write [file]");
+        println!("  write [file] [string]");
+        println!("  exit");
     } else if command == "ls" {
         let current = &shell.current.borrow();
-        ls(current);
+        println!("{}", ls(current));
+    } else if command == "cd" {
+        let change: Result<FileNodePointer, ()>;
+        {
+            let current = &shell.current.borrow();
+            let arg = *iter.next().unwrap();
+            if let Ok(pointer) = find(current, arg.to_string()) {
+                change = Ok(pointer.clone());
+            } else {
+                change = Err(());
+                println!("not found.");
+            }
+        }
+        if let Ok(change) = change {
+            shell.current = change;
+        }
+    } else if command == "find" {
+        let current = &shell.current.borrow();
+        let arg = *iter.next().unwrap();
+        if let Ok(pointer) = find(current, arg.to_string()) {
+            let node = &pointer.borrow();
+            let file = node.value();
+            let name = file.name();
+            println!("{}", name);
+        } else {
+            println!("not found.");
+        }
+    } else if command == "mkdir" {
+        let current = &mut shell.current.borrow_mut();
+        let arg = *iter.next().unwrap();
+        mkdir(current, arg.to_string());
+    } else if command == "touch" {
+        let current = &mut shell.current.borrow_mut();
+        let arg = *iter.next().unwrap();
+        touch(current, arg.to_string(), "".to_string());
+    } else if command == "read" {
+        let current = &shell.current.borrow();
+        let arg = *iter.next().unwrap();
+        if let Ok(pointer) = find(current, arg.to_string()) {
+            let node = &pointer.borrow();
+            let data = read(node);
+            println!("{}", data);
+        } else {
+            println!("not found.");
+        }
+    } else if command == "write" {
+        let current = &mut shell.current.borrow_mut();
+        let arg = *iter.next().unwrap();
+        let mut index =
+            buffer.rfind(arg).unwrap() +
+            arg.len();
+        for i in buffer[index..].chars() {
+            if i.is_whitespace() { index += 1; }
+            else { break; }
+        }
+        let data = &buffer[index..];
+        if let Ok(pointer) = find(current, arg.to_string()) {
+            let node = &mut pointer.borrow_mut();
+            write(node, data.to_string());
+        } else {
+            println!("not found.");
+        }
+    } else {
+        println!("{} command not found.", command);
     }
 }
 
 
 fn interactive() {
     println!("start interactive shell. Enjoy! :/");
-    println!("to stop, press Ctrl + c");
+    println!("to stop, press Ctrl + c or type exit");
     println!("if you need help, type :?");
 
     let root = FileNode::create_directory("".to_string(), Edge::new()).to_pointer();
