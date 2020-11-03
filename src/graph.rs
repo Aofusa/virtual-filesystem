3ecb39f67294f61b69f5995a585bb8cd37b4e916
Aofusa <5000enPCinAOFUSA@gmail.com>
2020-11-04 02:07:40 +0900

    update: テストの実装

--- src/graph.rs
+++ src/graph.rs
@@ -2,7 +2,7 @@ use std::rc::Rc;
 use std::cell::RefCell;
 
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Node<T>(pub T, pub Edge<T>);
 pub type NodePointer<T> = Rc<RefCell<Node<T>>>;
 pub type Edge<T> = Vec<NodePointer<T>>;
