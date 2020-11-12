bd0e2ce5dedf2d2173b129cb44d34ddf6888217c
Aofusa <5000enPCinAOFUSA@gmail.com>
2020-11-13 00:53:31 +0900

    update: wasmサンプル作成

--- example/virtual-filesystem-wasm/README.md
+++ example/virtual-filesystem-wasm/README.md
@@ -1,18 +1,32 @@
 Virtual Filesystem Wasm
 =====
 
-Virtual FileSystem の wasm 版
+
+Virtual FileSystem の wasm 版  
+
+
+ビルドやサーバの起動  
+-----
+
+以下のコマンドでサーバを起動し [localhost:8080](http://localhost:8080) にアクセスすると動作確認ができる  
+
+```sh
+# Cargo.toml のあるディレクトリで実行
+wasm-pack test --firefox --headless
+wasm-pack build
+
+# www 配下に移動して実行
+npm run start
+```
 
 
 初期構築  
+-----
+
 https://rustwasm.github.io/docs/book/game-of-life/hello-world.html
 
-ビルドやサーバの起動  
 ```sh
-wasm-pack test --firefox --headless
-wasm-pack build
 npm install
 # もしエラーが出たら npm i g node-gyp をして gyp を先に入れておく
-npm run start
 ```
 
--- example/virtual-filesystem-wasm/tests/web.rs
+++ example/virtual-filesystem-wasm/tests/web.rs
@@ -1,32 +1,258 @@
 extern crate virtual_filesystem_wasm;
-use virtual_filesystem_wasm::Universe;
+use virtual_filesystem_wasm::Cli;
 
 use wasm_bindgen_test::*;
 
-#[cfg(test)]
-pub fn input_spaceship() -> Universe {
-    let mut universe = Universe::new();
-    universe.set_width(6);
-    universe.set_height(6);
-    universe.set_cells(&[(1,2), (2,3), (3,1), (3,2), (3,3)]);
-    universe
-}
+#[wasm_bindgen_test]
+fn test_ls() {
+    let shell = &mut Cli::new();
 
-#[cfg(test)]
-pub fn expected_spaceship() -> Universe {
-    let mut universe = Universe::new();
-    universe.set_width(6);
-    universe.set_height(6);
-    universe.set_cells(&[(2,1), (2,3), (3,2), (3,3), (4,2)]);
-    universe
+    let buffer = "ls";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "ls a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "ls b c";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "ls";
+    assert_eq!(shell.run(buffer), "a".to_string());
+
+    let buffer = "ls a";
+    assert_eq!(shell.run(buffer), "a".to_string());
+
+    let buffer = "ls b c";
+    assert_eq!(shell.run(buffer), "a".to_string());
 }
 
 #[wasm_bindgen_test]
-pub fn test_tick() {
-    let mut input_universe = input_spaceship();
-    let expected_universe = expected_spaceship();
+fn test_pwd() {
+    let shell = &mut Cli::new();
 
-    input_universe.tick();
-    assert_eq!(&input_universe.get_cells(), &expected_universe.get_cells());
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/".to_string());
+
+    let buffer = "pwd a";
+    assert_eq!(shell.run(buffer), "/".to_string());
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "cd a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/a".to_string());
+}
+
+#[wasm_bindgen_test]
+fn test_cd() {
+    let shell = &mut Cli::new();
+
+    let buffer = "cd";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "cd a";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "cd b c";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/".to_string());
+
+    let buffer = "cd .";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/".to_string());
+
+    let buffer = "cd ..";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/".to_string());
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "cd a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "cd .";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/a".to_string());
+
+    let buffer = "mkdir b";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "cd b c";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "cd .";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/a/b".to_string());
+
+    let buffer = "cd ..";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/a".to_string());
+
+    let buffer = "cd /";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "pwd";
+    assert_eq!(shell.run(buffer), "/".to_string());
+}
+
+#[wasm_bindgen_test]
+fn test_find() {
+    let shell = &mut Cli::new();
+
+    let buffer = "find";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "find a";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "find b c";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "find a";
+    assert_eq!(shell.run(buffer), "a".to_string());
+
+    let buffer = "mkdir b";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "find b c";
+    assert_eq!(shell.run(buffer), "b".to_string());
+}
+
+#[wasm_bindgen_test]
+fn test_mkdir() {
+    let shell = &mut Cli::new();
+
+    let buffer = "mkdir";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "mkdir b c";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "mkdir a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "ls";
+    assert_eq!(shell.run(buffer), "a\tb\ta".to_string());
+}
+
+#[wasm_bindgen_test]
+fn test_touch() {
+    let shell = &mut Cli::new();
+
+    let buffer = "touch";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "touch a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "touch b c";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "touch a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "ls";
+    assert_eq!(shell.run(buffer), "a\tb\ta".to_string());
+}
+
+#[wasm_bindgen_test]
+fn test_read() {
+    let shell = &mut Cli::new();
+
+    let buffer = "read";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "read a b";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "touch a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "write a 123";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), "123".to_string());
+
+    let buffer = "read a b";
+    assert_eq!(shell.run(buffer), "123".to_string());
+
+    let buffer = "mkdir dir";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read dir";
+    assert_eq!(shell.run(buffer), format!("not file."));
+}
+
+#[wasm_bindgen_test]
+fn test_write() {
+    let shell = &mut Cli::new();
+
+    let buffer = "write";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "write a";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "write a 123";
+    assert_eq!(shell.run(buffer), format!("not found."));
+
+    let buffer = "touch a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "write a";
+    assert_eq!(shell.run(buffer), format!("illegal argument."));
+
+    let buffer = "write a 123";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), "123".to_string());
+
+    let buffer = "write a 123 456 xyz";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "read a";
+    assert_eq!(shell.run(buffer), "123123 456 xyz".to_string());
+
+    let buffer = "mkdir dir";
+    assert_eq!(shell.run(buffer), "".to_string());
+
+    let buffer = "write dir string";
+    assert_eq!(shell.run(buffer), format!("not file."));
 }
 
