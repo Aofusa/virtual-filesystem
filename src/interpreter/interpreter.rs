c0a6b0ab56ed03c93578a1b3b69d7d9df5e6c68b
Aofusa <5000enPCinAOFUSA@gmail.com>
2020-11-14 04:01:29 +0900

    update: 加算減算の実装

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -1,24 +1,52 @@
 
-pub type InterpreterResult<'a> = Result<Option<&'a str>, InterpreterError>;
+pub type InterpreterResult<'a> = Result<Option<String>, InterpreterError>;
 
 #[derive(Debug, PartialEq)]
 pub enum InterpreterError {
     Unknown,
 }
 
-pub fn interpret<'a>(s: &'a str) -> InterpreterResult<'a> {
-    Ok(Some(s))
+fn split_digit(s: &str) -> (&str, &str) {
+    let first_non_num = s.find(|c| !char::is_numeric(c)).unwrap_or(s.len());
+    s.split_at(first_non_num)
+}
+
+pub fn interpret(s: &str) -> InterpreterResult {
+    let mut stack = 0;
+    let mut p = s.chars();
+
+    let (n, _) = split_digit(p.as_str());
+    for _ in 0..n.len() { p.next(); }
+    stack = stack + n.parse::<i32>().unwrap();
+
+    loop {
+        match p.next() {
+            None => break,
+            Some('+') => {
+                let (n, _) = split_digit(p.as_str());
+                for _ in 0..n.len() { p.next(); }
+                stack = stack + n.parse::<i32>().unwrap();
+            },
+            Some('-') => {
+                let (n, _) = split_digit(p.as_str());
+                for _ in 0..n.len() { p.next(); }
+                stack = stack - n.parse::<i32>().unwrap();
+            },
+            _ => { panic!("unreachable here") }
+        }
+    }
+
+    Ok(Some(format!("{}", stack)))
 }
 
 #[cfg(test)]
-mod test {
+mod tests {
     use crate::interpreter::interpreter::interpret;
 
     #[test]
     fn test_interpreter() {
-        let s = "any";
-
-        assert_eq!(interpret(s), Ok(Some(s)));
+        assert_eq!(interpret("42"), Ok(Some("42".to_string())));
+        assert_eq!(interpret("5+20-4"), Ok(Some("21".to_string())));
     }
 }
 
