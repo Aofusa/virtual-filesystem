52317fec2319cf9c0209fd03d9303212e281d7bb
Aofusa <5000enPCinAOFUSA@gmail.com>
2020-11-18 22:30:01 +0900

    update: 複数引数での関数呼び出し

--- src/interpreter/interpreter.rs
+++ src/interpreter/interpreter.rs
@@ -103,6 +103,7 @@ mod tests {
         assert_eq!(x.interpret(": $a = 100; return $a / 10"), Ok(Some("10".to_string())));
         assert_eq!(x.interpret(": $a = abs; $b = 'drf'; $c = \"123\""), Ok(Some("123".to_string())));
         assert_eq!(x.interpret(":; :"), Ok(Some("".to_string())));
+        assert_eq!(x.interpret(": a b c"), Ok(Some("c".to_string())));
     }
 }
 
